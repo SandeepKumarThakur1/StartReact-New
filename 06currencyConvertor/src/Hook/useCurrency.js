8269d6d5557bbc5c61570931df91faa488e82b67
sandeep Kumar <sandeepkumarthakur9888@gmail.com>
2025-02-03 22:34:25 +0530

    Changes in Currency Convertor

--- 06currencyConvertor/src/Hook/useCurrency.js
+++ 06currencyConvertor/src/Hook/useCurrency.js
@@ -3,13 +3,12 @@ import { useEffect, useState } from "react";
 const useCurrency = (currency) => {
   const [data, setData] = useState({});
   useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-    )
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
-  }, [currency]);
-  console.log(data);
+      console.log(data);
+    }, [currency]);
+    console.log(data);
   return data;
 };
 
